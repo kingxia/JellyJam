690b2fa9fd274db8de49d687fb5b881c549a82a1
LeRoi <kxhere@gmail.com>
2017-04-06 20:20:28 -0700

    Add enemy and AI framework

---
+++ JellyJam/Entities/Behaviors/Behavior.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+
+/**
+ * Interface for AI behavior.
+ */
+namespace JellyJam.Entities.Behaviors {
+  public interface Behavior {
+    GameAction getAction();
+    Vector2 getMove();
+  }
+}
+
+public enum GameAction {
+  Move,
+  Attack
+}
---
+++ JellyJam/Entities/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using JellyJam.Entities.Behaviors;
+using Microsoft.Xna.Framework;
+
+namespace JellyJam.Entities {
+  public class Enemy : Entity {
+    private Behavior behavior;
+
+    public Enemy(string animation, Vector2 position, Behavior behavior) :
+      base(animation, position) {
+      this.behavior = behavior;
+    }
+  }
+}
