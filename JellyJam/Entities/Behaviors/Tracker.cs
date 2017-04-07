ef9f061c41446b2e430b7a12269e8dffc6ed4fc9
LeRoi <kxhere@gmail.com>
2017-04-06 20:50:01 -0700

    Add sample behavior and entity update loop

---
+++ JellyJam/Entities/Behaviors/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+/**
+ * Behavior that takes the shortest path towards a target Entity.
+ */
+namespace JellyJam.Entities.Behaviors {
+  public class Tracker : Behavior {
+    private Entity target;
+
+    public Tracker(Entity target) {
+      this.target = target;
+    }
+
+    public GameAction getAction() {
+      return GameAction.Move;
+    }
+
+    public Vector2 getMove() {
+      return Vector2.Zero;
+    }
+  }
+}
--- JellyJam/Entities/Enemy.cs
+++ JellyJam/Entities/Enemy.cs
@@ -4,10 +4,21 @@ using Microsoft.Xna.Framework;
 namespace JellyJam.Entities {
   public class Enemy : Entity {
     private Behavior behavior;
+    private int speed = 4;
 
     public Enemy(string animation, Vector2 position, Behavior behavior) :
       base(animation, position) {
       this.behavior = behavior;
+    }
+
+    public override void update(float elapsedTime) {
+      GameAction action = behavior.getAction();
+      if (action == GameAction.Move) {
+        Vector2 destination = behavior.getMove();
+        position = destination;
+      }
+
+      base.update(elapsedTime);
     }
   }
 }
