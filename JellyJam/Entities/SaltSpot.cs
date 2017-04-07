a27565581c1d37b685cdf40d2701f4e86917e471
Martin Wozniewicz <martin.woz@gmail.com>
2017-04-07 08:03:41 -0700

    Renaming for clarity

--- JellyJam/Entities/SaltSpot.cs
+++ JellyJam/Entities/SaltSpot.cs
@@ -4,33 +4,31 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace JellyJam.Entities {
   public class SaltSpot : Entity {
-    private readonly Vector2 _startPos;
+
+    private float currentTime = 0;
+
     private readonly Vector2 _endPos;
 
-    private float interval = 1f / 30;
-    private float currentTime = 0;
-    private Vector2 step;
-    private int steps = 3;
+    // Animation for throwing the salt
+    // Time between totalThrowSteps, "speed"
+    private float stepInterval = 1f / 30;
+    private Vector2 stepVector;
+    private int totalThrowSteps = 3;
 
 
     public SaltSpot(string animation, Vector2 startPos, Vector2 endPos) : base(animation, startPos) {
-      _startPos = startPos;
       _endPos = endPos;
-      step = Vector2.Subtract(_endPos, _startPos);
-      step = Vector2.Divide(step, steps);
+      stepVector = Vector2.Subtract(_endPos, startPos);
+      stepVector = Vector2.Divide(stepVector, totalThrowSteps);
     }
 
     public override void Update(float elapsedTime) {
       currentTime += elapsedTime;
-      if (steps > 0 && currentTime > interval) {
-        position = Vector2.Add(position, step);
-        steps--;
+      if (totalThrowSteps > 0 && currentTime > stepInterval) {
+        base.position = Vector2.Add(base.position, stepVector);
+        totalThrowSteps--;
       }
       base.Update(elapsedTime);
-    }
-
-    public override void Draw(SpriteBatch spriteBatch) {
-      base.Draw(spriteBatch);
     }
   }
 }
